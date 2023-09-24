0ed3710f2333b6a7b3d24a165dce8253cfcbe421
Ted van Riel <80752652+tvanriel@users.noreply.github.com>
2023-09-24 09:28:26 +0200

    feat: shortcut to fx logging

--- logging/fx.go
+++ logging/fx.go
@@ -1,5 +1,16 @@
 package logging
 
-import "go.uber.org/fx"
+import (
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+)
 
 var Module = fx.Module("logging", fx.Provide(NewZapLogger))
+
+
+func FXLogger() fx.Option {
+        return fx.WithLogger(func(l *zap.Logger) fxevent.Logger {
+		return &fxevent.ZapLogger{Logger: l}
+	})
+
+}
