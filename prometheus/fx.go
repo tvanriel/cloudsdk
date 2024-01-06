316f0e6818569053dce027e907debd3c6ff856c3
Ted van Riel <80752652+tvanriel@users.noreply.github.com>
2024-01-06 14:08:25 +0100

    fix: add prometheus to fx lc instead of invoke

--- prometheus/fx.go
+++ prometheus/fx.go
@@ -2,4 +2,6 @@ package prometheus
 
 import "go.uber.org/fx"
 
-var Module = fx.Module("prometheus", fx.Invoke(Listen), fx.Provide(NewPrometheus))
+var Module = fx.Module("prometheus", 
+        fx.Provide(NewPrometheus),
+)
--- prometheus/prometheus.go
+++ prometheus/prometheus.go
@@ -1,38 +1,57 @@
 package prometheus
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	io_prometheus_client "github.com/prometheus/client_model/go"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 type Prometheus struct {
 	registry   *prometheus.Registry
 	listenAddr string
+        Log *zap.Logger
 }
 
-func NewPrometheus(config Configuration) *Prometheus {
+func NewPrometheus(config Configuration, log *zap.Logger, lc fx.Lifecycle) *Prometheus {
 
 	registry := prometheus.NewRegistry()
+        l := log.Named("prometheus")
 
 	registry.MustRegister(
 		collectors.NewGoCollector(),
 		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
 	)
 
-	return &Prometheus{
+        prom := &Prometheus{
 		registry:   registry,
 		listenAddr: config.Address,
 	}
-}
+        
+        lc.Append(fx.Hook{
+                OnStart: func(ctx context.Context) error {
+                        l.Info("listen address", zap.String("addr", prom.listenAddr))
 
-func Listen(prom *Prometheus) {
+                        go func() {
+
 	srv := http.NewServeMux()
 	srv.Handle("/metrics", promhttp.HandlerFor(prom.registry, promhttp.HandlerOpts{Registry: prom.registry}))
-	go http.ListenAndServe(prom.listenAddr, srv)
+
+                err := http.ListenAndServe(prom.listenAddr, srv)
+                if err != nil {
+                                        l.Error("Failed to open metricslistener", zap.Error(err))
+                }
+                        }()
+                        return nil
+                },
+        })
+        return prom
+
 }
 
 func (p *Prometheus) Register(c prometheus.Collector) error {
